6999f7e59b68dbd8e18c6aea9e81d849ae850df4
Pamela Frieze <pfrieze@gmail.com>
2019-02-07 20:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function findMatching(drivers, string) {
   drivers.filter(function (string) string => driver == string)
 }
+
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
