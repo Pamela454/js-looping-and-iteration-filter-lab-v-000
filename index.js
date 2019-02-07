ef5af833cf12964f6802ddd86c595ba72a49010a
Pamela Frieze <pfrieze@gmail.com>
2019-02-07 20:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  return drivers.filter(function (driver) string => driver == string)
+  return drivers.filter(function (driver) { 
+    return driver.toLowerCase() === string.toLowerCase();
+  });
 }
 
 function findMatching (list, name) {
