2ba0c27c1164c88a7f52c9e876b3087a683162fb
Pamela Frieze <pfrieze@gmail.com>
2019-02-07 20:41:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function fuzzyMatch(drivers, string) {
 
 function matchName(drivers, string) {
   return drivers.filter(function (driver) { 
-    return driver[0,1] === string[0,1];
+    return driver.name === string;
   });
 }
